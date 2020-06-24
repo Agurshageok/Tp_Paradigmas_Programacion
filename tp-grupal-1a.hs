1c4d4409fcd7c5a6b1ed65ddfc09de50c5386d7f
Macalui00 <39487444+Macalui00@users.noreply.github.com>
2020-06-24 13:54:10 -0300

    Nuevas funciones pero sigo sin probar nada
    Me quede trabaja en algunas funciones, no se me ocurre como armarlas.

--- tp-grupal-1a.hs
+++ tp-grupal-1a.hs
@@ -33,7 +33,38 @@ main = do
 -- Orde Lexicográfico
 procesarArchivo :: [String] -> DataSet -> DataSet
 procesarArchivo [] y = []
-procesarArchivo (x:xs) y = -- Si x = "char - numero" entonces Hoja else Nodo x (procesarArchivo xs y)
+procesarArchivo xs y = -- si esRama xs y then ***** insertarRama xs (buscarArbol xs y) else generarArbolNuevo xs y
+
+--- ***** aca faltaria ver como volver a la lista de arboles sin generar dos arboles repetidos
+
+esRama :: [String] -> DataSet -> Bool
+esRama xs [] = False
+esRama xs (y:ys) = if esrama2 xs y then True else False
+
+-- Suponiendo que esta verificado que todos tienen la misma longitud?
+esRama2 :: [String] -> ArbolNivel -> Bool
+esRama2 (x:[]) (c,n) = sonIguales (split2 x) (c,n) -- si son iguales que retorne true si no que retorne false (?)
+esRama2 (x:xs) (c,n) = True -- Suponemos que llegamos a la hoja del arbol pero la lista de string es una rama mas larga entonces es verdadero igual
+esRama2 (x:xs) (Nodo c (y:ys))= if x notContains x "-" then (if x == c then esRama2 xs ys else False) else True -- Si es hoja el elemento del string significa que llegue al final de la rama y es una rama del arbol mas chica, por ende verdadero.
+
+-- notContains deberia verificar que x no tiene el - que indicaria que seria la hoja
+
+-- Es igual la hoja del string que la hoja del arbol?
+sonIguales :: [String] -> Hoja -> Bool
+sonIguales (x:[]) (c,n) = if x == n then True else False
+sonIguales (x:xs) (c,n) = if x == c then sonIguales xs (c,n) else False
+
+--busco el arbol al que pertenece la rama
+buscarArbol :: [String] -> DataSet -> ArbolNivel 
+buscarArbol xs (y:ys) = if esRama2 xs y then y else buscarArbol xs ys
+
+--inserto la rama nueva al arbol pre-existente
+insertarRama :: [String] -> ArbolNivel -> ArbolNivel -- Me enquilombe al pensar como insertarlo
+insertarRama (x:xs) (Nodo c (y:ys)) = if x notContains x "-" then (if x == c then (if nodoDe y == x then Nodo c (insertarRama xs (y:ys)) else Nodo c (y:  ) 
+
+generarArbolNuevo :: [String] -> DataSet -> DataSet
+generarArbolNuevo (x:xs) [] =  Nodo x -- idem ya me queme para pensarlo
+generarArbolNuevo (xs) (y:ys) = (y: generarArbolNuevo xs ys)
 
 type Hoja = (Char,Int) 
 data ArbolNivel = Nodo Char [ArbolNivel] | Hoja
@@ -53,6 +84,13 @@ split :: String -> [String] -- EN TEORIA esto convierte un string en una lista d
 split "" = []
 split xs = ys : (split . drop 1) zs
    where (ys, zs) = span (/=',') xs
+
+split2 :: String -> [String] -- EN TEORIA esto convierte un string en una lista de string, Ejemplo: cadena "la luna,el sol,a,b,c", se convierte en ["la luna","el sol","a","b","c"]
+split "" = []
+split xs = ys : (split . drop 1) zs
+   where (ys, zs) = span (/='-') xs -- Convierto la hoja qeu estaba en string en una lista de string para manipular los caracteres
+
+--span, aplicado a un predicado p y una lista xs, devuelve una tupla donde el primer elemento es el prefijo más largo (posiblemente vacío) de xs de elementos que satisfacen py el segundo elemento es el resto de la lista
 
 -- main :: IO ()
 -- main = do
